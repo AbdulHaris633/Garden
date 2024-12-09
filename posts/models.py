15e0f768538a7cb31c6759224ba00331dc9e2da7
AbdulHaris633 <haris611@outlook.com>
2024-12-10 01:50:37 +0500

    Alter model name

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from users.models import User
 import uuid
 
-class Post (models.Model):   
+class UserPost(models.Model):    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,)
+    image = models.ImageField(upload_to='images/',null=True)  
+    description = models.TextField(max_length=100,null=True) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)  
+    
+class Articles(models.Model):  
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='images/')
-    description = models.TextField(max_length=100) 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    url= models.CharField(max_length=1000)      
+      
+     
     
     
