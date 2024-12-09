c2e338c04ea61c58a2dc931ef9f79e70b1e78e1c
AbdulHaris633 <haris611@outlook.com>
2024-12-10 01:51:35 +0500

    Refactor Serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import *
 
-class PostSerializer(serializers.ModelSerializer):
+class UserPostSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False)
     user = serializers.CharField(required=False)
     
     class Meta:   
-         model = Post
-         fields = "__all__" 
+         model = UserPost
+         fields = "__all__"   
+         
+class ArticleSerilizer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+     
+    class Meta:   
+         model = Articles
+         fields = "__all__"    
